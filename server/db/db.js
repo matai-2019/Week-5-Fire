3520b97ffbb3d85d9376a141257b2bbea7068e34
Noel Soong <noelsoong@live.com>
2019-06-28 10:42:44 +1200

    export getLies and add Lie from db.js

--- server/db/db.js
+++ server/db/db.js
@@ -3,28 +3,11 @@ const config = require('../../knexfile')[environment]
 const connection = require('knex')(config)
 
 module.exports = {
-  getWidgets,
-  saveWidget,
-  deleteWidget
-}
-
-function getWidgets(db = connection) {
-  return db('widgets').select()
-}
-
-function saveWidget(widget, db = connection) {
-  return db('widgets')
-    .insert({
-      name: widget.name,
-      price: widget.price,
-      mfg: widget.mfg,
-      inStock: widget.inStock,
-      rating: widget.rating
-  })
-}
-
-function deleteWidget (id, db = connection) {
-  return db('widgets')
-    .where('id', id)
-    .delete()
+  getlies(db = connection) {
+    return db('lies').select()
+  },
+  addLie(newLie, db=connection){
+    return db('lies')
+    .insert(newLie)
+  }
 }
