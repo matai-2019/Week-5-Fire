566ba6991023620d6a140a7e3f9f908cf12c8e55
Ruslan Makeev <dec32m@gmail.com>
2019-06-28 11:12:22 +1200

    Board component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import { Container } from 'semantic-ui-react'
 import AppHeader from './components/AppHeader'
+import Board from './components/Board'
 
 function App() {
   return (
     <>
       <Container>
         <AppHeader />
+        <Board />
       </Container>
     </>
   )
