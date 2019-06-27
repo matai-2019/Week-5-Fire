1f204de04a50c1cb8b16f15bc63e437e481fc457
Jared Tasker <jared@needapart.co.nz>
2019-06-28 11:29:09 +1200

    Add TrumpCard component

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import { Container, Grid, GridColumn } from 'semantic-ui-react';
+import TrumpCard from './TrumpCard';
 
 class Board extends React.Component {
   render() {
     return (
-      <div>
-        <h1> Board check </h1>
+      <div className = "Board">
+        <p>Board</p>
+        <Container>
+          <Grid columns = {3}>
+            <GridColumn>
+              <TrumpCard />
+            </GridColumn>
+            <GridColumn>
+              <TrumpCard />
+            </GridColumn>
+            <GridColumn>
+              <TrumpCard />
+            </GridColumn>
+          </Grid>
+        </Container>
       </div>
-
     )
   }
 }
