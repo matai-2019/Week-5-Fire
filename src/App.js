95ef78e8b4436fb4d481825c88198b844075f7b7
Jared Tasker <jared@needapart.co.nz>
2019-06-28 12:32:05 +1200

    Make app staeful

--- src/App.js
+++ src/App.js
@@ -3,16 +3,30 @@ import './App.css'
 import { Container } from 'semantic-ui-react'
 import AppHeader from './components/AppHeader'
 import Board from './components/Board'
+import { stat } from 'fs';
 
-function App() {
-  return (
-    <>
-      <Container>
-        <AppHeader />
-        <Board />
-      </Container>
-    </>
-  )
+class App extends React.Component {
+  state = {
+    score: 0,
+    quotes: [],
+    lie: []
+  }
+
+  componentDidMount () {
+    //connect with db and api
+  }
+
+  render () {
+    return (
+      <>
+        <Container>
+          <AppHeader />
+          <Board />
+        </Container>
+      </>
+    )
+  }
 }
+
 
 export default App
