b7192601e4027af672c9d23a02e6421f7a18f84f
Noel Soong <noelsoong@live.com>
2019-06-28 15:01:17 +1200

    Pass down cards to board

--- src/components/TrumpGame.jsx
+++ src/components/TrumpGame.jsx
@@ -2,14 +2,39 @@ import React, { Component } from 'react'
 import Board from './Board'
 
 export class TrumpGame extends Component {
-  state = {
-    score: 0,
-    quotes: [],
-    lie: []
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      score: 0,
+      quotes: [{
+        message: 'card1',
+        value: 'trump'
+      }, {
+        message: 'card2',
+        value: 'trump'
+      }, {
+        message: 'card3',
+        value: 'trump'
+      }],
+      lies: [{
+        message: 'lie1',
+        value: 'lie'
+      }, {
+        message: 'li1e',
+        value: 'lie'
+      }, {
+        message: 'lie3',
+        value: 'lie'
+      }],
+    }
   }
+
+
   render() {
+    const { quotes, lies } = this.state
     return (
-      <Board />
+      <Board quotes={quotes} lies={lies} />
     )
   }
 }
