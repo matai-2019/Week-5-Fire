f1105cd27f1aa2e4e73443cb04ecd298eab6b50c
Jared Tasker <jared@needapart.co.nz>
2019-06-28 13:59:21 +1200

    add Trump game component

--- src/App.js
+++ src/App.js
@@ -3,29 +3,21 @@ import { Route } from 'react-router-dom'
 import './App.css'
 import { Container } from 'semantic-ui-react'
 import AppHeader from './components/AppHeader'
-import Board from './components/Board'
 import FinalPage from './components/FinalPage'
 import { stat } from 'fs';
+import TrumpGame from './components/TrumpGame';
 
 class App extends React.Component {
-  state = {
-    score: 0,
-    quotes: [],
-    lie: []
-  }
-
   componentDidMount () {
-    //connect with db and api
+    //connect with db and api pass down to TrumpGame
   }
-
-
 
   render () {
     return (
       <>
         <Container>
           <AppHeader />
-          <Board />
+          <TrumpGame />
         </Container>
         <Route path='/final' exact component={FinalPage} />
       </>
